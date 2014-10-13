4ba1fa0c7d19e240a4f8bba0086cd5bb864f8633
Heather Praiswater <heathered@gmail.com>
2014-10-13 13:53:39 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hwtracker::Application.config.secret_token = '37fb9e190a3bde84c57518ae241bc31f216ad4c8d9ca52617ed17b2f7abd192c270bcb2e76c69d407ecf2ddda88a0bcfd45ce76af9c230870960a82edd6d1279'
