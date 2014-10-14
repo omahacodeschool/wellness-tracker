6e21feb6da98cb13b396161752b027085ec89364
Heather Praiswater <heathered@gmail.com>
2014-10-14 11:24:23 -0500

    Events needed user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141014152748) do
+ActiveRecord::Schema.define(:version => 20141014162325) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "metrics", :force => true do |t|
